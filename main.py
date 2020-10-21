cd27a87b4c4e15296d918042e2bf8b8ffedda5bc
Finn <finnbutler1@gmail.com>
2020-10-21 22:14:32 +0100

    basic commandline program

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import argparse
 
-print("Hello")
+currentarg = [""]
+
+while currentarg[0] != "quit":
+    currentarg = input("fit-track: ").split()
+    if currentarg[0] == "help":
+        print("HELP COMMANDS HERE !!!")
+    elif currentarg[0] == "track":
+        if len(currentarg) < 3:
+            print("Invalid syntax")
+        elif currentarg[1] == "-w":
+            print("Recording weight of "+currentarg[2])
+        elif currentarg[1] == "-o":
+            print("Recording run of "+currentarg[2] + " in time "+currentarg[3])
+    elif currentarg[0] == "graph":
+        print("show graph")
